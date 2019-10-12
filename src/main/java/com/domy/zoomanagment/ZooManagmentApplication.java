55dfcf44bf01cf67bfb05ec606bd614d528f2e55
meisster <gabara.krzysztof@gmail.com>
2019-10-12 13:48:11 +0200

    Initial commit

---
+++ src/main/java/com/domy/zoomanagment/ZooManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.domy.zoomanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZooManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domy/zoomanagment/ZooManagmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.domy.zoomanagment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZooManagmentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
